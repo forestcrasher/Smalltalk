557c4c7a84972907a5ad59fd27f2ff03a6341666
forestcrahser <forestcrasher@outlook.com>
2020-12-01 00:06:31 +0300

    Add and configure pods

--- Podfile
+++ Podfile
@@ -1,10 +1,17 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '12.0'
+use_frameworks!
+inhibit_all_warnings!
 
 target 'Smalltalk' do
-  # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-
-  # Pods for Smalltalk
-
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+  pod 'RxDataSources', '~> 4.0'
+  pod 'Swinject', '2.7.0'
+  pod 'SwinjectAutoregistration', '2.7.0'
+  pod 'Cartography', '~> 3.0'
+  pod 'R.swift'
+  pod 'SwiftLint'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Firestore'
+  
 end
--- Smalltalk/ViewController.swift
+++ Smalltalk/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.textColor = .black
+        label.center = view.center
+        label.textAlignment = .center
+        view.addSubview(label)
+
+        let db = Firestore.firestore()
+        db.collection("test").getDocuments { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                label.text = querySnapshot?.documents.first?.data()["text"] as? String
+            }
+        }
     }
 
-
 }
-
